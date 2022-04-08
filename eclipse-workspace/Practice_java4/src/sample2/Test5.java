b068712d90fc9c4e958daa3f1b108a8fc00495f6
jisrat19 <jisrat19@gmail.com>
2022-04-07 22:29:19 -0400

    Five

---
+++ eclipse-workspace/Practice_java4/src/sample2/Test5.java
@@ -0,0 +1,11 @@
+package sample2;
+
+public class Test5 {
+	public static void main(String [] args) {
+		System.err.println("Test 5");
+	}
+	
+	
+	
+	
+}
---
+++ eclipse-workspace/Practice_java4/src/sample2/Test6.java
@@ -0,0 +1,11 @@
+package sample2;
+
+public class Test6 {
+
+	public static void main(String [] args) {
+		System.err.println("Test 6");
+	}	
+	
+	
+	
+}
