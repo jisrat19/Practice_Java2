ecb7c540284aec47c8ff0672f995ff22f5bae877
jisrat19 <jisrat19@gmail.com>
2022-04-07 22:11:15 -0400

    Third

---
+++ eclipse-workspace/Practice_Java2/src/Test1.java
@@ -0,0 +1,10 @@
+
+public class Test1 {
+
+	public static void main(String [] args) {
+		System.err.println("Test 1");
+	}
+	
+	
+	
+}
---
+++ eclipse-workspace/Practice_Java2/src/Test2.java
@@ -0,0 +1,10 @@
+
+public class Test2 {
+	public static void main(String [] args) {
+		System.err.println("Test 2");
+	}
+	
+	
+	
+	
+}
